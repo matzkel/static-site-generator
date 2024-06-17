ba6317db4abccf95a22797c44ccb3e7d5ca20199
matzkel <matzkel@proton.me>
2024-06-17 18:57:11 +0300

    Add function that converts TextNode to LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from htmlnode import LeafNode
+
+
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -15,3 +18,18 @@ class TextNode():
         if self.url:
             return f"TextNode('{self.text}', {self.text_type}, '{self.url}')"
         return f"TextNode('{self.text}', {self.text_type})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == "text":
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == "bold":
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == "italic":
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == "code":
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == "link":
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == "image":
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError("text_node.text_type is required to be of appropriate value")
