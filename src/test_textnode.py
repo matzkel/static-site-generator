855a5ffb2aad244dd3d041a1da79eb5b2f8c31c1
matzkel <matzkel@proton.me>
2024-06-19 14:48:17 +0300

    Add unittests for function that converts text node to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -13,6 +13,14 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         diff_node = TextNode("This is a different text node", "normal")
         self.assertNotEqual(node, diff_node)
+    
+    def test_to_html_node(self):
+        node = TextNode("This is a text node", "bold")
+        self.assertEqual(str(text_node_to_html_node(node)), "HTMLNode('<b>', 'This is a text node')")
+
+    def test_to_html_node_value_error(self):
+        node = TextNode("This text has wrong type.", "something")
+        self.assertRaises(ValueError, text_node_to_html_node, node)
 
 
 if __name__ == "__main__":
